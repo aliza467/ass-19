46cfdac17091e5b4b3e071218a8875238f656491
aliza <ak6614462@gmail.com>
2024-03-31 05:32:16 +0500

    assignment 19

---
+++ app.js
@@ -0,0 +1,93 @@
+// ASSIGNMENT 19
+
+// CHAPTER 26
+
+// q1
+// console.log(Math.round(4.6))
+
+// q2
+// origNum=5.55;
+// roundNum=Math.round(origNum)
+// console.log(roundNum)
+
+// q3
+// origNum=5.55;
+// roundNum=Math.floor(origNum)
+// console.log(roundNum)
+
+// q4
+// a=3.88;
+// b=Math.round(a)
+// console.log(b);
+
+// q5
+// origNum=0.5;
+// roundNum=Math.floor(origNum)
+// console.log(roundNum)
+
+// CHAPTER 27
+// q1
+// a=Math.random()*50
+// b=Math.floor(a)
+// console.log(b);
+
+// q2
+// a=Math.random()
+// console.log(a);
+
+
+// q3
+// dice=Math.random()*6+1
+// round=Math.floor(dice)
+// console.log("you get number",round,"on your dice")
+
+
+// q4
+// coin=Math.random()
+// round=Math.round(coin)
+// if(round==0){
+//     console.log("HEADS");
+// }
+// else{
+//     console.log("TAILS");
+// }
+
+// CHAPTER 28,29
+// q1
+// var str=42
+// var int=+str
+// console.log(int);
+
+// q2
+//  var str="123"
+//  var int=+str
+//  console.log(int);
+
+// q3
+// var a="2.12"
+// var b=parseFloat(a)
+// console.log(b);
+
+// q4
+// var number = 42;
+// var str = number.toString();
+// console.log(str);
+
+// q5
+// var decimalString = "3.14";
+// var integerValue = +(decimalString);
+// console.log(integerValue)
+
+// CHAPTER 30
+// q1
+// var num = 3.141592653589793238;
+// var newNum = num.toFixed(4);
+// console.log(newNum)
+
+// q2
+// var num = 3.141592653589793238;
+// num = parseFloat(num.toFixed(2));
+// console.log(num);
+
+
+
